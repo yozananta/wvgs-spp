0c4f0f91aa60ceb60a55dee7c40cdfdab779eaaa
yozananta <92283172+yozananta@users.noreply.github.com>
2023-07-01 03:42:04 +0700

    koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi=mysqli_connect('localhost','root','','db_spp');
+$koneksi=mysqli_connect('bkjmhjymwtdlxr23jdgl-mysql.services.clever-cloud.com','ugzsfexfl9h2ra2e','bkjmhjymwtdlxr23jdgl','bkjmhjymwtdlxr23jdgl');
 
 	if(mysqli_connect_error()){
 		printf("Connect Failed:", mysqli_connect_error());
